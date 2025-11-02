b918fcb2d0bfcf21c5c45f43af895dd082978905
habiba247668 <habiba.elmallah05@gmail.com>
2025-11-02 02:49:34 -0800

    Checkpoint

---
+++ My project/Assets/LevelManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    public GameObject CurrentCheckpoint;
+    public Transform player;
+    void Start()
+    {
+        CurrentCheckpoint= null;
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    public void RespawnPlayer(){
+        FindObjectOfType <PlayerControl> ().transform.position = CurrentCheckpoint.transform.position;
+    }
+
+
+}
